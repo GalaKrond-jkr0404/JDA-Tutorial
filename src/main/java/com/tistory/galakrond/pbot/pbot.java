0a4b66b987079aefa56e671dddd2f9cf0199cdfb
Galakrond-jkr0404 <jdng56600403@gmail.com>
2022-09-02 15:58:36 +0900

    intial commit

---
+++ src/main/java/com/tistory/galakrond/pbot/pbot.java
@@ -0,0 +1,15 @@
+package com.tistory.galakrond.pbot;
+
+import net.dv8tion.jda.api.sharding.DefaultShardManager;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+
+public class pbot {
+    public pbot() {
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault("INSERT_BOT_TOKEN");
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
