f054b9e0635cc49bb98a703640dff4512269e0a0
GalaKrond-jkr0404 <jdng56600403@gmail.com>
2022-09-06 23:29:19 +0900

    made env

--- src/main/java/com/tistory/galakrond/pbot/pbot.java
+++ src/main/java/com/tistory/galakrond/pbot/pbot.java
@@ -1,5 +1,6 @@
 package com.tistory.galakrond.pbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManager;
@@ -12,14 +13,20 @@ import java.util.stream.StreamSupport;
 public class pbot {
 
     private final ShardManager shardManager;
+    private final Dotenv config;
 
     public pbot() throws LoginException { // 잠재적으로 LoginExecption을 발생시킬 수 있다.
-        String token = "MTAxNTE1MTc3Nzk3ODUxOTYxMw.GFZS2l.fPBSQyqZTTRuTGmMbqMha3nthBV1WxDkfqjQGY";
+        config = Dotenv.configure().ignoreIfMissing().load();
+        String token = config.get("TOKEN");
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("Galakrond"));
         shardManager = builder.build();
 
+    }
+
+    public Dotenv getConfig(){
+        return config;
     }
 
     public ShardManager getShardManager() {
@@ -31,7 +38,7 @@ public class pbot {
             pbot bot = new pbot();
         }
         catch (LoginException e){
-            System.out.println("Error occured In Login session. did you check the token?"); //ret
+            System.out.println("Error occured In Login session. did you check the token?"); //retr
         }
     }
 
